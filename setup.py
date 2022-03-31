3cebef9e428234dbf04d132b334e491965307e5b
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2022-03-31 23:31:10 +0200

    fix: typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='foundrycli',
     packages=find_packages(),
     version='0.1.0',
-    description='Use `forge` and `cast` commmands in Python scripts',
+    description='Use `forge` and `cast` commands in Python scripts',
     url="https://github.com/ZeroEkkusu/foundrycli.py",
     author='Zero Ekkusu',
     license='MIT/Apache',
